2cd5dc71351c685d6b3d8e25a08f226dcb5c8e70
Robert Sam <robert.sam@amalitech.org>
2022-01-06 14:13:55 +0000

    Setting Files Up

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import "./App.css";
-
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>Let's Get Started.</h1>
+    </div>
+  );
 }
 
 export default App;
